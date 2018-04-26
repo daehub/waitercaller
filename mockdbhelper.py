3a113ca86aaab081830c4d4d9dac54ef3af902fa
RULTR <daehub@126.com>
2018-04-26 22:17:44 +0800

    Add register function

--- mockdbhelper.py
+++ mockdbhelper.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Daehub'
 
-MOCK_USERS = {'test@example.com': '123456'}
+MOCK_USERS = [{'email':'test@example.com','salt':"""b'uQuP0/D9kL/oY/N46zlToYd/ayo='""",'hashed':'24b183ec5c404b60d002ee6244c6b55bbac65267662a2ef6100293ab99997f829a3c6b90cd58a1abee87acbd8702edcfbe885b4a335689bf43ee1333cd9e0df5'}]
 
 
 class MockDBHelper(object):
     def get_user(self,email):
-        if email in MOCK_USERS:
-            return MOCK_USERS[email]
+        user = [x for x in MOCK_USERS if x.get('email')==email]
+        if user:
+            return user[0]
         return None
+
+    def add_user(self,email,salt,hashed):
+        MOCK_USERS.append({'email':email,'salt':salt,'hashed':hashed})
--- waitercaller.py
+++ waitercaller.py
@@ -11,29 +11,35 @@ from flask import url_for
 from flask_login import LoginManager
 from flask_login import login_required
 from flask_login import login_user
+from flask_login import logout_user
 from user import User
 from mockdbhelper import MockDBHelper as DBHelper
+from passwordhelper import PasswordHelper
 
 app = Flask(__name__)
 login_manager = LoginManager(app)
 DB = DBHelper()
+PH = PasswordHelper()
 app.secret_key = 'ypzuLGRoE/pFeD/MyElW9N9HU/Qugm4ctGJHRDUb0vT4fRCtYDP4n6nOTAIGDgjQVgn6GhzM0D5R4ZOdmpCGgVV9E4hW0aCQml5'
 
+
 @app.route('/')
-def home(error_message = None):
+def home(error_message=None):
     return render_template('home.html',error_message = error_message)
+
 
 @app.route('/account')
 @login_required
 def account():
     return "You are logged in !"
 
-@app.route('/login',methods=['POST'])
+
+@app.route('/login', methods=['POST'])
 def login():
     email = request.form.get("email")
     password = request.form.get("password")
-    user_password = DB.get_user(email)
-    if user_password and user_password == password:
+    stored_user = DB.get_user(email)
+    if stored_user and PH.validate_password(password,stored_user['salt'].encode('utf-8'),stored_user['hashed']):
         user = User(email)
         login_user(user)
         return redirect(url_for('account'))
@@ -41,11 +47,33 @@ def login():
     return home()
 
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form.get('email')
+    pw1 = request.form.get('password')
+    pw2 = request.form.get('password2')
+    if not pw1 == pw2 :
+        return redirect(url_for('home'))
+    if DB.get_user(email):
+        return redirect(url_for('home'))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1+str(salt))
+    DB.add_user(email,salt,hashed)
+    return redirect(url_for('home'))
+
+
 @login_manager.user_loader
 def load_user(user_id):
     user_password = DB.get_user(user_id)
     if user_password:
         return User(user_id)
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
